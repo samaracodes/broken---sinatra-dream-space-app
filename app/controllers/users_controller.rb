f46827f828d6de5dbdd19d04f55c24bdbb06f57a
samara-amanda <samara.amanda01@gmail.com>
2020-12-07 04:03:07 -0500

    fixed bug in post login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
-    # what routes do I need for login?
-    # purpose of this route is to show(render) the user the login oage/form.
+        # what routes do I need for login?
+        # purpose of this route is to show(render) the user the login oage/form.
 
     get '/login' do
         erb :login
     end
 
-    #this purpose of this route is to receive the login form
-    # find the user and log the user in (creste a session)
+        #this purpose of this route is to receive the login form
+        # find the user and log the user in (creste a session)
     post '/login' do
             # params looks like: {email: "user@user.com", password: "password""}
             # Find the user
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
             #Authenticaste the user - verify the user is who they say they are
             # they have the credentials - email/password combo
             
-        if @user.authenticate(params[:password])
+        if @user && @user.authenticate(params[:password])
                 # log the user in - create the user session
                 
             session[:user_id] = @user.id #this is what is actually logging the user in
@@ -23,18 +23,19 @@ class UsersController < ApplicationController
             puts session
             redirect "/users/#{@user.id}"
         else
-                # tell the user they entered invalid credentials
-                # redirect them to the login page
-                "You do not have an account, try again."
+                    # tell the user they entered invalid credentials
+                    # redirect them to the login page
+            flash[:message] = "The email and/or password entered was not a match. Please try again."
+            redirect '/login'
         end
 
 
     end
 
 
-    # this route's job is to render the signup form
+        # this route's job is to render the signup form
     get '/signup' do
-        #erb (render) a view
+            #erb (render) a view
         erb :signup
     end
 
@@ -56,15 +57,15 @@ class UsersController < ApplicationController
                 # where do I go now?
             redirect to "/users/#{@user.id}"
         else
-            # not valid input
+                # not valid input
             redirect to '/signup'
-            #it would be ideal to display a message
+                #it would be ideal to display a message
         end
     end
 
     #user SHOW route
     get '/users/:id' do
-        #what do I need to do first?
+            #what do I need to do first?
         @user = User.find_by(id: params[:id])
 
         erb :'/users/show'
