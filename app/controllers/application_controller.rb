369c9418e3cab0f4a371e24c0f732091273e2d78
samara-amanda <samara.amanda01@gmail.com>
2020-12-07 03:59:29 -0500

    registered Sinatra Flash in configure block

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 require './config/environment'
 
+
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
+    register Sinatra::Flash
     set :session_secret, "our_awesome_dream_space_app"
     #if someone doesnt have the sesssion scret and tries 
     # to imitate you they wont be able to.
